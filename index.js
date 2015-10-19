4ffca92b165b2ea5386f9b80b3cdfe50d0968a33
Flavio Li Volsi <flavio.livolsi@gmail.com>
2015-10-19 22:37:10 +0200

    index.js refactored

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
-var femaleNames = require('./female-names.json');
-var maleNames = require('./male-names.json');
+
+var names = {
+  f: require('./female-names.json'),
+  m: require('./male-names.json')
+};
 
 exports.guess = function(name) {
   name = name.trim().toLowerCase();
-  if (maleNames.indexOf(name) >= 0) {
-  	return 'm';
-  }
-  if (femaleNames.indexOf(name) >= 0) {
-  	return 'f';
+  for (var gender in names) {
+    if (names[gender].indexOf(name) > -1)
+      return gender;
   }
 };
+
